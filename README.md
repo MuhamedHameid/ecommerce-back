0374f6ea4a3d381d2b2b949a83783ba794843ab5
MuhamedHameid <113292572+MuhamedHameid@users.noreply.github.com>
2023-01-01 14:15:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# E-COMMERCE Back-End
+
+Hi Friends, @muhamedhameid here
+This is my first back-end code in my life and not the last ISA, If you find my code clean please don't forget to give me a **_star_**.
+
+# Pattern
+
+I have use **MVC pattern** to build this project, so code become more readable, consist and clean.
+
+## End-Points in this project
+
+- Products
+- Categories
+- Customers
+- Admin
+- Orders
+- Complaints
+
+## How to use this back-end code
+
+- First clone the code
+- run `> npm install`
+- create .env file and add **DB_URL=your Atlas MongoDB likee**
---
+++ server.js
@@ -0,0 +1,54 @@
+//Express
+const express = require("express");
+const app = express();
+
+//Essential Middle Wares & Enviroment
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+require("dotenv").config();
+
+//Middlewares
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Database Connection Setup
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL, {
+    // Recovery
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => console.log("Error in DB Connectin" + err));
+
+//PORT
+const PORT = process.env.PORT || 3009;
+
+//Routes
+const ProuctsRoute = require("./routes/Products");
+const CategoriesRoute = require("./routes/Categories");
+const OrdersRoute = require("./routes/Orders");
+const UsersRoute = require("./routes/Users");
+const ComplaintsRoute = require("./routes/Complaints");
+const AdminRoute = require("./routes/Admins");
+const LodinRoute = require("./routes/login");
+const DashboardRoute = require("./routes/dashboard");
+
+app.use("/api/product", ProuctsRoute);
+app.use("/api/category", CategoriesRoute);
+app.use("/api/order", OrdersRoute);
+app.use("/api/user", UsersRoute);
+app.use("/api/complain", ComplaintsRoute);
+app.use("/api/admin", AdminRoute);
+app.use("/api/login", LodinRoute);
+app.use("/api/dashboard", DashboardRoute);
+
+
+//Server RUNNING
+app.listen(PORT, () =>
+  console.log(`Server run on PORT http://localhost:${PORT}`)
+);
